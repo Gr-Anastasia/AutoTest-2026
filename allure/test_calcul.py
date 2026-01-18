50f6bc9c5a6d94e3c279b8f44b8a00f088985e58
Nastya Gr <proekt28n@gmail.com>
2026-01-18 14:07:05 +0500

    Dz 12.01.2026 add assert in test suit

--- allure/test_calcul.py
+++ allure/test_calcul.py
@@ -1,22 +1,32 @@
 import allure
 
+
 @allure.title("Сложение в калькуляторе")
 @allure.description("Сложение в калькуляторе, где a=10,b=5")
 def test_addition(a=10,b=5):
     with allure.step("Сложение 10 + 5"):
-        print(a + b)
+        result = a + b
+        print(result)
+
+    assert result == 15, f'10 + 5 = 15, а в результате {result}'
 
 @allure.title("Вычитание в калькуляторе")
 @allure.description("Вычитание в калькуляторе, где a=10,b=5")
 def test_subtraction(a=10,b=5):
     with allure.step("Вычитание 10 - 5"):
-        print(a - b)
+        result = a - b
+        print(result)
+
+    assert result == 5, f'10 - 5 = 5, а в результате {result}'
 
 @allure.title("Умножение в калькуляторе")
 @allure.description("Умножение в калькуляторе, где a=10,b=5")
 def test_multiplication(a=10,b=5):
     with allure.step("Умножение 10 * 5"):
-        print(a * b)
+        result = a * b
+        print(result)
+
+    assert result == 50, f'10 * 5 = 50, а в результате {result}'
 
 @allure.title("Деление в калькуляторе")
 @allure.description("Деление в калькуляторе, где a=10,b=0")
@@ -26,4 +36,8 @@ def test_division(a=10,b=0):
             print("На 0 делить нельзя")
             return None
         else:
-            print(a // b)
+            result = a // b
+            print(result)
+
+        assert result is None, 'При деление на 0, результат = None'
+
